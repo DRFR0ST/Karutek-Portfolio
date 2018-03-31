1614ecbdae7e6c4e3dd4a8a307c9dc4e4af0a366
Mike Eling <mike.eling97@gmail.com>
2018-03-31 18:19:12 +0200

    🎨 Header and About components improved

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -5,14 +5,16 @@ class About extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			icon: props.icon
+			icon: props.icon,
+			animated: props.animated
 		}
 	}
 
 	render() {
 		return (
-			<div class="about">
+			<div class={`about ${this.state.animated ? "animated" : ""}`}>
 				{this.state.icon ? <Icon>{this.state.icon}</Icon> : ""}
+				<h5>About me</h5>
 				<p>{this.props.children}</p>
 			</div>);
 	}
